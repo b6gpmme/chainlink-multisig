e98cfd8cf5b0917e6d0ffbeecbcb35e37a51f0d6
Tyler <tylerpeairs@protonmail.com>
2021-06-02 10:10:04 -0500

    Frontend can display ETH and USD balance on button click

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,23 @@
 import { ethers } from 'ethers';
-import React, { Component } from "react";
+import React, { useState } from "react";
 import logo from './logo.svg';
+import Web3 from 'web3';
 import './App.css';
+
 
 import MultiSigWallet from "./artifacts/contracts/MultiSigWallet.sol/MultiSigWallet.json";
 
 const mswAddress =  "0x6DE282021D7a1b9c8377f61BF374AAa08b6c5DB9";
-
+const LinkAddress = "0xa36085f69e2889c224210f603d836748e7dc0088"
+const web3 = new Web3(Web3.givenProvider);
 // This is an error code that indicates that the user canceled a transaction
 //const ERROR_CODE_TX_REJECTED_BY_USER = 4001;
 
-class App extends Component {
-  state = {balance: undefined, loaded:false};
-
-  componentDidMount = async () => {
-    try {
-      //Get network provider and web3 instance
-      this.web3 = await getWeb3();
-      // Use web3 to get the user's accounts
-      this.accounts = await this.web3.eth.getAccounts();
-
-      // Get contract instance
-      const networkId = await this.web3.eth.net.getId();
-
-      this.multisigWallet = new this.web3.eth.Contract(
-        MultiSigWallet.abi,
-        MultiSigWallet.networks[networkId] && MultiSigWallet.networks[networkId].address,
-      );
-      this.setState({loaded:true});
-    } catch (error) {
-      alert(
-        'Failed to load web3, accounts, or contract. Check console for details.',
-        );
-        console.error(error);
-      }
-    };
+function App() {
+  var [tokenData, setTokenData] = useState();
+  var [tokenBalance, setTokenBalance] = useState();
+  var [balanceState, setBalanceState] = useState();
+  var [balanceUSDState, setBalanceUSDState] = useState();
   
   //request access to user MetaMask
   //async function requestAccount(){
@@ -42,32 +25,27 @@ class App extends Component {
   //}
 
   //get Balance of MultiSigWallet
-  getBalance = async () => {
-    const balance = await multisigWallet.getBalance().then((data) => {
-            console.log('Balance is: ', web3.utils.fromWei(data.toString(), "ether"), "ETH");
-        });
-        this.setState({[balance]: balance});
-  }
-  //async function getBalance() {
-  //  if (typeof window.ethereum !== 'undefined') {
-  //      const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //      const contract = new ethers.Contract(mswAddress,MultiSigWallet.abi, provider)
-  //      const balance = await contract.getBalance();
-  //      console.log("Balance: ", balance.toString());
-  //      this.setState({balance});
-  //      };
-  //  }
-  render() {
-      if (!this.state.loaded) {
-        return <div>Loading Web3, accounts, and contract...</div>;
-      }
-    return (
-      <div className="App">
-        <header className="App-header">
-            <button onClick={getBalance}>Get Balance</button>
-        </header>
-      </div>
-    )};
+  async function getBalance() {
+    if (typeof window.ethereum !== 'undefined') {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const contract = new ethers.Contract(mswAddress,MultiSigWallet.abi, provider)
+        const balance = await contract.getBalance();
+        const balanceUSD = await contract.getBalanceInUSD();
+        //console.log("Balance: ", balance.toString());
+        setBalanceState(web3.utils.fromWei(balance.toString(), "ether") + " ETH");
+        setBalanceUSDState(balanceUSD.toString() + " USD");
+        };
+    }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+          <button onClick={getBalance}>Get Balance</button>
+          <p>{balanceState}</p>
+          <p>{balanceUSDState}</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
